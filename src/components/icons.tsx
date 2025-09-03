3e5c613a3e87b47349e52d9914773721b31bbe20
Omji <om.exe12345@gmail.com>
2025-09-03 16:09:54 +0000

    the image is still not loading

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export const Logo = () => (
-    <Image src="https://images.sharda.ac.in/brand-logo/sharda-university-logo.png" alt="Sharda University Logo" width={300} height={85} />
+    <Image src="/sharda-university-logo.png" alt="Sharda University Logo" width={300} height={85} />
   );
